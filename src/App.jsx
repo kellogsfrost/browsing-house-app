f8fe8fd728008ecd20580ee450085f0d96570aa9
abdella206 <f.abdella1@yahoo.com>
2019-07-29 14:42:08 -0700

    created the app jsx page and set it up

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import Home from './Home'
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+
+class App extends React.Component {
+  state = {
+
+  }
+  render() {
+    return (
+
+
+      <>
+        <Home />
+
+        <Router>
+        <nav>
+          <Link  to='/'>HOME</Link>{' '}
+          <Link  to='/city'>CityList</Link>{' '}
+          <Link  to='/show'>Show</Link>{''}
+        </nav>
+        {/* <Route exact path='/' component={Home} />
+        
+        <Route exact path='/city' render={() => <  />} />
+
+      
+        <Route exact path='/show'/> */}
+
+
+
+      </Router>
+
+
+      </>
+
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default App;
