de1881610df7361994cd2e48f9d8203422d98abc
abdella206 <f.abdella1@yahoo.com>
2019-07-29 15:44:03 -0700

    created the axios.get

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Home from './Home'
+import axios from 'axios';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -10,11 +11,21 @@ import {
 
 class App extends React.Component {
   state = {
-    cityList: {},
-    schoolList: {}
+    cityList: '',
+    schoolList: '',
   }
 
 
+  
+  componentDidMount() {
+    axios.get('https://api.placeilive.com/v1/houses/search?q=newyork%204')
+      .then(result => {
+        console.log(result)
+        this.setState({
+          issues: result.data
+        })
+      })
+  }
 
   
 
