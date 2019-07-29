27d3ba9eb748951dbecc3ab222e8ae0a039571b7
rivedroz23 <river.droz@gmail.com>
2019-07-29 14:39:14 -0700

    changed the name of file

---
+++ src/NeighborhoodList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NeighborhoodList = props => (
+<>
+<button>Go!<div className="manhatten">Manhatten</div></button>
+<button>Go!<div className="brooklyn">Brooklyn</div></button>
+</>
+  
+)
+
+
+export default NeighborhoodList;
