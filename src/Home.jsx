bb82a279acc271ff7b032c3f8e4b8d1bb591d990
rivedroz23 <river.droz@gmail.com>
2019-07-29 15:43:30 -0700

    updated home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,9 +3,14 @@ import React, { Component } from 'react'
 export class Home extends Component {
     render() {
         return (
+            <>
             <div>
-                <h1>Test!!!</h1>
+                <h3>NYC | London | San Fransisco | Berlin</h3>
             </div>
+           <div>
+               <h4>Queens, Brooklyn, Manhatten and Staten Island </h4>
+            </div>
+           </>
         )
     }
 }
